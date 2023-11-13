faa58be433f6445db23803264fc7e96974456e28
Ben <btb4516@g.rit.edu>
2023-11-13 16:15:05 -0500

    class

--- adminer/displaypages.php
+++ adminer/displaypages.php
@@ -1,17 +1,72 @@
 <?php
-include("dbConn.php");
+/**
+ * Connect to the database.
+ */
 
-$sql = "SELECT * FROM `240Pages` LIMIT 50";
-$result = $conn->query($sql);
+	include("../../../dbConn.php");
 
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    //echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-    echo $row['page'] . "<br/>" . $row['content'] . "<hr/>";
-  }
-} else {
-  echo "0 results";
-}
-$conn->close();
+
+	//figure out state of program...should i be nserting something ???
+	if( !empty( $_GET['fName'] ) && !empty( $_GET['lName'] ) ){
+		$fName = $_GET['fName'];
+		$lName = $_GET['lName'];
+		//die("DATA PROVIDED INSERT...");
+		//$sql= "INSERT INTO `240Insert` (`first`, `last`) VALUES ('JP', 'Taylor');";
+		
+		// $sql= "INSERT INTO `240Insert` (`first`, `last`) VALUES ('$fName', '$lName');";
+		//echo "<br/> . $sql . "<br/>;
+		// $conn->query($sql);
+		
+		$sql = "INSERT INTO `240Insert` (`first`, `last`) VALUES (?, ?);";
+		$stmt = $conn->prepare($sql);
+		$stmt->bind_param("ss",$fName, $lName);
+		$stmt->execute();
+
+
+	
+	}
+
+
+
+	$sql = "SELECT * FROM `240Insert` LIMIT 50";
+
+	$result = $conn->query($sql);
+
+	if ($result->num_rows > 0) {
+		// output data of each row
+		while($row = $result->fetch_assoc()) {
+			$names[] = $row;
+		}
+	}
+
+
+
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>240 DB Insert</title>
+	</head>
+	<body>
+		<h3>The List</h3>
+		<div id="list">
+			<ul>
+			<?php
+				//will use for output
+				foreach ($names as $names) {
+				  echo "<li>" . $names['first'] . ' '. $names['last'] . "</li>" ;
+				}
+			?>
+			</ul>
+		</div>
+		<hr/>
+		<h3>Add to the list</h3>
+		<form action="preparedStatement.php" method="get">		
+			First name: <input type="text" id="first" name="fName" />
+			Last name: <input type="text" id="last" name="lName"/>
+			<input type="submit" value="Add to the List"/>
+		</form>
+	</body>
+</html>

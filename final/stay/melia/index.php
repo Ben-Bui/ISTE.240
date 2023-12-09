08d53895958fcb5749db0123bd4d9a35471961d3
Ben Bui <Benthebui@gmail.com>
2023-12-09 02:54:28 -0500

    melia

--- final/stay/melia/index.php
+++ final/stay/melia/index.php
@@ -3,55 +3,50 @@ $pageTitle = "Melia";
 $welcometitle = "Melia";
 include ("../../header.php");
 ?>
-    <figure>
-        <img class="picture" src="melia_london_kensington.jpg" id="pic" alt="pic">
-    </figure>
-<!-- This place seem to be highend, so should suggest to people with more money to spare -->
-    <div class="info">
-            <h1>Why stay at Melia London Kensington a Melia Collection Hotel</h1>
-            <p> The hotel is located in the Kensington area of London, which is known for its cultural attractions, 
-                including  museums such as the Victoria and Albert Museum and the Natural History Museum. 
-                It's also well-connected to public transportation, making it easy to explore the city.<br>
+<figure>
+    <img class="picture" src="melia_london_kensington.jpg" id="pic" alt="pic">
+</figure>
 
-                The Meliá London Kensington known for it contemporary and stylish design, creating a comfortable and visually 
-                pleasing environment for guests. The hotel offers a range of room types to suit different needs, from 
-                standard rooms to spacious suites. Whether you're traveling solo, with a partner, or as a family, you 
-                can find the right accommodation.<br>
-                
-                The hotel provides a selection of amenities, likefitness center, restaurant, and meeting rooms. 
-                These amenities can better your stay and cater to various requirements. Meliá hotels are known for their 
-                commitment to service and guest satisfaction. The staff aims to make your stay enjoyable and hassle-free so they say on the website.<br>
-                
-                The hotel's restaurant offers a variety of dishes, and you can experience both international and British cuisine without having to leave the premises. 
-                The Meliá London Kensington often provides good value for the quality of accommodations and services offered, making it a cost-effective choice 
-                for travelers.<br>
-                
-                The hotel's location is ideal for exploring Kensington's cultural attractions, as well as nearby landmarks like Hyde Park and the Royal Albert Hall.
-                Meliá hotels usually offer flexible booking options, allowing you to adjust your stay according to your travel plans.<br>
-                
-                But in the end, it is up to you if this is the place you want to stay, please refer down below for rating and past guests review to have more view on Whether
-                or not this is the place you want to be.
-            </p>
-    </div>
-    <!-- snap shot of the map -->
-    <figure>
-        <img class="picture" src="meliamap.JPG" id="map" alt="map">
-    </figure>
-    <!-- General contact infromation and website, also add Address and phone number -->
+<div class="info">
+    <h1>Melia London Kensington: A Luxurious Stay</h1>
+    <h2>Location and Accessibility</h2>
+    <p>The hotel is situated in London's Kensington area, renowned for its cultural landmarks like the Victoria and Albert Museum and the Natural History Museum. Its convenient access to public transportation simplifies city exploration.</p>
 
-        <fieldset>
-            <legend>How to Contact Them!!</legend>        
-            <ul class="formelement">
-                <li> <a href="https://www.tripadvisor.com/Hotel_Review-g186338-d199140-Reviews-Melia_London_Kensington_member_of_Melia_Collection-London_England.html">
-                    Trip Advisor Critic Rating</a></li>
-                <li> <a href="https://www.melia.com/en/hotels/united-kingdom">
-                    Their Home Website</a></li>
-                <li> <a href="https://www.google.com/travel/search?q=meli%C3%A1%20london%20kensington%20reviews&g2lb=2502548%2C2503771%2C2503781%2C4258168%2C4270442%2C4284970%2C4291517%2C4308226%2C4570332%2C4597339%2C4757164%2C4814050%2C4874190%2C4893075%2C4924070%2C4965990%2C4990494%2C72248047%2C72248048%2C72298667%2C72302247%2C72317059%2C72321071%2C72333916%2C72370225%2C72372541%2C72379816%2C72386302%2C72390435&hl=en-US&gl=us&cs=1&ssta=1&qs=CAEyE0Nnb0lyb0dMb3FXdXQ3QUdFQUU4AkIJCa7AQlRy3WAGQgkJrsBCVHLdYAY&ap=ugEGcHJpY2Vz&ictx=1&ved=0CAAQ5JsGahcKEwio7N33sfKBAxUAAAAAHQAAAAAQCw&ts=CAESCAoCCAMKAggDGkYKKBImMiQweDQ4NzYwNTVlNmZkNjQ5NjU6MHg2NjBkZDcyNTQ0MmMwYWUSGhIUCgcI6A8QARgTEgcI6A8QARgUGAEyAhAA">
-                    Reservation</a></li>
-            </ul>
-            <p>Address: 61 Gloucester Rd, South Kensington, London SW7 4PE, United Kingdom</p>
-            <p>Phone Number: 020 7437 9585</p>
-            <p>Email: info@quovadissoho.co.uk</p>
-        </fieldset>
-</body>
-</html>
+    <h2>Modern Design and Accommodations</h2>
+    <p>The Meliá London Kensington boasts contemporary and stylish design, offering diverse room types to cater to individual preferences, from standard rooms to spacious suites.</p>
+
+    <h2>Facilities and Service</h2>
+    <p>With amenities like a fitness center, restaurant, and meeting rooms, the hotel prioritizes guest comfort and satisfaction. The staff is dedicated to ensuring a pleasant and hassle-free stay.</p>
+
+    <h2>Dining Experience</h2>
+    <p>The hotel's restaurant serves a variety of international and British dishes, providing guests an opportunity to relish culinary delights without leaving the premises.</p>
+
+    <h2>Prime Location for Exploration</h2>
+    <p>Adjacent to cultural attractions in Kensington and landmarks such as Hyde Park and the Royal Albert Hall, the hotel offers an ideal base for sightseeing.</p>
+
+    <h2>Flexible Booking Options</h2>
+    <p>Meliá hotels usually offer flexible booking options to accommodate varying travel plans, ensuring convenience for guests.</p>
+
+    <h2>Guest Ratings and Reviews</h2>
+    <p>For more insights, refer to the hotel's ratings and reviews on:
+        <ul>
+            <li><a href="https://www.tripadvisor.com/Hotel_Review-g186338-d199140-Reviews-Melia_London_Kensington_member_of_Melia_Collection-London_England.html">Trip Advisor</a></li>
+            <li><a href="https://www.google.com/travel/search?q=meli%C3%A1%20london%20kensington%20reviews&g2lb=2502548%2C2503771%2C2503781%2C4258168%2C4270442%2C4284970%2C4291517%2C4308226%2C4570332%2C4597339%2C4757164%2C4814050%2C4874190%2C4893075%2C4924070%2C4965990%2C4990494%2C72248047%2C72248048%2C72298667%2C72302247%2C72317059%2C72321071%2C72333916%2C72370225%2C72372541%2C72379816%2C72386302%2C72390435&hl=en-US&gl=us&cs=1&ssta=1&qs=CAEyE0Nnb0lyb0dMb3FXdXQ3QUdFQUU4AkIJCa7AQlRy3WAGQgkJrsBCVHLdYAY&ap=ugEGcHJpY2Vz&ictx=1&ved=0CAAQ5JsGahcKEwio7N33sfKBAxUAAAAAHQAAAAAQCw&ts=CAESCAoCCAMKAggDGkYKKBImMiQweDQ4NzYwNTVlNmZkNjQ5NjU6MHg2NjBkZDcyNTQ0MmMwYWUSGhIUCgcI6A8QARgTEgcI6A8QARgUGAEyAhAA">Google Reviews</a></li>
+        </ul>
+    </p>
+</div>
+
+<figure>
+    <img class="picture" src="meliamap.JPG" id="map" alt="map">
+</figure>
+
+<fieldset>
+    <legend>Contact Information</legend>
+    <ul class="formelement">
+        <li><a href="https://www.melia.com/en/hotels/united-kingdom">Hotel's Official Website</a></li>
+        <li><a href="https://www.google.com/travel/search?q=meli%C3%A1%20london%20kensington%20reviews&g2lb=2502548%2C2503771%2C2503781%2C4258168%2C4270442%2C4284970%2C4291517%2C4308226%2C4570332%2C4597339%2C4757164%2C4814050%2C4874190%2C4893075%2C4924070%2C4965990%2C4990494%2C72248047%2C72248048%2C72298667%2C72302247%2C72317059%2C72321071%2C72333916%2C72370225%2C72372541%2C72379816%2C72386302%2C72390435&hl=en-US&gl=us&cs=1&ssta=1&qs=CAEyE0Nnb0lyb0dMb3FXdXQ3QUdFQUU4AkIJCa7AQlRy3WAGQgkJrsBCVHLdYAY&ap=ugEGcHJpY2Vz&ictx=1&ved=0CAAQ5JsGahcKEwio7N33sfKBAxUAAAAAHQAAAAAQCw&ts=CAESCAoCCAMKAggDGkYKKBImMiQweDQ4NzYwNTVlNmZkNjQ5NjU6MHg2NjBkZDcyNTQ0MmMwYWUSGhIUCgcI6A8QARgTEgcI6A8QARgUGAEyAhAA">Reservation</a></li>
+    </ul>
+    <p>Address: 61 Gloucester Rd, South Kensington, London SW7 4PE, United Kingdom</p>
+    <p>Phone Number: 020 7437 9585</p>
+    <p>Email: info@quovadissoho.co.uk</p>
+</fieldset>

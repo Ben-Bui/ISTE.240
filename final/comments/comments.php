3213a638a64cc82f248f7880e2f63fd15f28cc48
Ben Bui <Benthebui@gmail.com>
2023-12-09 01:55:12 -0500

    change

--- final/comments/comments.php
+++ final/comments/comments.php
@@ -11,29 +11,29 @@
             background-color: red;
             transition: background-color 0.5s ease-in-out;
         }
+        /* CSS for Image Gallery */
+        .gallery {
+            display: flex;
+            flex-wrap: wrap;
+        }
+        .gallery img {
+            width: 150px;
+            margin: 5px;
+            cursor: pointer;
+            transition: opacity 0.5s ease-in-out;
+        }
+        .gallery img:hover {
+            opacity: 0.7;
+        }
     </style>
     <script>
-        function validateForm() {
-            var name = document.getElementById("name").value;
-            var email = document.getElementById("email").value;
-            var comment = document.getElementById("comment").value;
-
-            if (name.trim() === '' || comment.trim() === '') {
-                alert("Name and Comment fields are required.");
-                return false;
+        // JavaScript for Image Gallery
+        function displayImage(imageId) {
+            var images = document.getElementsByClassName("gallery-img");
+            for (var i = 0; i < images.length; i++) {
+                images[i].style.display = "none";
             }
-
-            // You can add more advanced validation here if needed
-            
-            return true;
-        }
-
-        function changeColor() {
-            var dynamicElement = document.getElementById("dynamicElement");
-            dynamicElement.style.backgroundColor = "blue";
-            setTimeout(function() {
-                dynamicElement.style.backgroundColor = "red";
-            }, 2000);
+            document.getElementById(imageId).style.display = "block";
         }
     </script>
 </head>
@@ -41,7 +41,7 @@
     <h1>Leave a Comment</h1>
     <!-- Comment Form -->
     <form action="process_comment.php" method="post" onsubmit="return validateForm()">
-        <label for="name">Name:</label>
+    <label for="name">Name:</label>
         <input type="text" id="name" name="name" required><br><br>
 
         <label for="email">Email:</label>
@@ -50,17 +50,17 @@
         <label for="comment">Comment:</label><br>
         <textarea id="comment" name="comment" rows="4" cols="50" required></textarea><br><br>
 
-        <!-- You can add more fields if needed -->
+        <!-- Add more fields if needed -->
         
         <input type="submit" value="Submit" onclick="changeColor()">
     </form>
 
     <hr>
-    
+
     <h2>Previous Comments</h2>
     <!-- Display Previous Comments -->
     <?php
-    include("../../../dbConn.php"); 
+    include("../../../dbConn.php"); // Include your database connection file
 
     $sql = "SELECT * FROM `comments` LIMIT 50";
     $result = $conn->query($sql);
@@ -77,8 +77,25 @@
 
     $conn->close();
     ?>
-    
     <!-- DHTML Component -->
     <div id="dynamicElement"></div>
+    
+    <!-- Image Gallery -->
+    <div class="gallery">
+        <img class="gallery-img" src="image1.jpg" alt="Image 1" onclick="displayImage('image1')">
+        <img class="gallery-img" src="image2.jpg" alt="Image 2" onclick="displayImage('image2')">
+        <img class="gallery-img" src="image3.jpg" alt="Image 3" onclick="displayImage('image3')">
+    </div>
+
+    <!-- Images for Gallery -->
+    <div id="image1" class="gallery-img" style="display: none;">
+        <img src="image1.jpg" alt="Image 1">
+    </div>
+    <div id="image2" class="gallery-img" style="display: none;">
+        <img src="image2.jpg" alt="Image 2">
+    </div>
+    <div id="image3" class="gallery-img" style="display: none;">
+        <img src="image3.jpg" alt="Image 3">
+    </div>
 </body>
 </html>

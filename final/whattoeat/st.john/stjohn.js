2674ee41d4942e692c821c1e096eed45d44412f5
Ben Bui <Benthebui@gmail.com>
2023-12-10 00:19:43 -0500

    slideshow

--- final/whattoeat/st.john/stjohn.js
+++ final/whattoeat/st.john/stjohn.js
@@ -1,28 +1,33 @@
 let currentImage = 0;
 const images = document.querySelectorAll('.gallery img');
+const intervalTime = 3000; // Change the interval time (in milliseconds) as needed
+let slideInterval;
 
 function showImage(n) {
     images.forEach(img => img.style.display = 'none');
     images[n].style.display = 'block';
 }
 
-function prevImage() {
-    if (currentImage > 0) {
-        currentImage--;
-    } else {
-        currentImage = images.length - 1;
-    }
+function nextImage() {
+    currentImage = (currentImage + 1) % images.length;
     showImage(currentImage);
 }
 
-function nextImage() {
-    if (currentImage < images.length - 1) {
-        currentImage++;
-    } else {
-        currentImage = 0;
-    }
-    showImage(currentImage);
+function startSlide() {
+    slideInterval = setInterval(nextImage, intervalTime);
+}
+
+function stopSlide() {
+    clearInterval(slideInterval);
 }
 
 // Show the first image initially
 showImage(currentImage);
+
+// Start the slideshow
+startSlide();
+
+// Optional: Stop the slideshow on mouseover and start on mouseout
+const gallery = document.querySelector('.gallery');
+gallery.addEventListener('mouseover', stopSlide);
+gallery.addEventListener('mouseout', startSlide);
